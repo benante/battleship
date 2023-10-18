3f9183ebcc215462fe7fc0b45b38d8a23a4f997f
isobelbutler <isobutler@icloud.com>
2023-10-18 16:47:33 +0100

    slightly tidier

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { placeShip } from '../utils/placeShip.jsx';
+import { placeShip } from '../utils/placeShip';
 import ComputerBoard from './ComputerBoard';
 import PlayerBoard from './PlayerBoard';
 import PlaceShipButton from './PlaceShipsButton';
@@ -9,27 +9,23 @@ import '../../public/styles/ships.css';
 import '../../public/styles/board.css';
 
 export default function Game() {
-  // Define the initial state for computer and player boards
   const [computerBoard, setComputerBoard] = useState(
     Array.from({ length: 10 }, () => Array(10).fill(null))
   );
-
   const [playerBoard, setPlayerBoard] = useState(
     Array.from({ length: 10 }, () => Array(10).fill(null))
   );
-
   const [shipsPlaced, setShipsPlaced] = useState(false);
-  // const [hitComputerCellCount, setComputerHitCellCount] = useState(0);
-  // const [hitPlayerCellCount, setPlayerHitCellCount] = useState(0);
 
   const handlePlaceShips = () => {
     // Place ships on the computer's board
-    placeShip(computerBoard, 4, 3); // Adjust the ship length as needed
-    setComputerBoard([...computerBoard]); // Trigger a state update for the computer's board
+    placeShip(computerBoard, 4, 3);
+    setComputerBoard([...computerBoard]);
 
     // Place ships on the player's board
-    placeShip(playerBoard, 4, 3); // Adjust the ship length as needed
-    setPlayerBoard([...playerBoard]); // Trigger a state update for the player's board
+    placeShip(playerBoard, 4, 3);
+    setPlayerBoard([...playerBoard]);
+
     setShipsPlaced(true);
   };
 
