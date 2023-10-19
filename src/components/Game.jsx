00c0a87238c104246d82b99e72fa0312c675717d
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-10-19 13:41:11 +0100

    computer seems working

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { placeShip } from '../utils/placeShip';
 import ComputerBoard from './ComputerBoard';
 import PlayerBoard from './PlayerBoard';
 import PlaceShipButton from './PlaceShipsButton';
 import ScoreBoard from './ScoreBoard';
 import isEqual from 'lodash/isEqual';
-
-
 import '../../public/styles/ships.css';
 import '../../public/styles/board.css';
 import { selectCells } from '../utils/computerSelection';
@@ -19,6 +17,7 @@ export default function Game() {
     Array.from({ length: 10 }, () => Array(10).fill(null))
   );
   const [shipsPlaced, setShipsPlaced] = useState(false);
+  const [selectedPlayerCell, setSelectedPlayerCell] = useState(null);
 
   const handlePlaceShips = () => {
     // Place ships on the computer's board
@@ -31,78 +30,31 @@ export default function Game() {
 
     setShipsPlaced(true);
   };
-  const [selectedPlayerCell, setSelectedPlayerCell] = useState(null);
 
   const handleCellClick = (clickedBoard, rowIndex, colIndex) => {
-    // Conditionally select a cell if it's playerBoard
-    const selectedCell = clickedBoard === playerBoard ? selectCells(clickedBoard) : { coordX: rowIndex, coordY: colIndex };
-
-    // Now you can safely access the coordinates
-    const { coordX, coordY } = selectedCell;
-    // setTimeout(() => {
-
-    //   console.log("Printing coords from Game:");
-    //   console.log(selectedCell)
-    // }, 1000);
-
-    // Check if the selected cell is a ship ('B')
-    if (clickedBoard[coordX][coordY] === 'B') {
-      // Update the selected cell state to 'hit' for a hit
-      // and update the board state
-      clickedBoard[coordX][coordY] = 'hit';
-    } else {
-      // Update the selected cell state to 'miss' for a miss
-      // and update the board state
-      clickedBoard[coordX][coordY] = 'miss';
-    }
-
-    // Update the board state based on which board is being clicked
     if (isEqual(clickedBoard, computerBoard)) {
-      setPlayerBoard([...clickedBoard]);
+      // If it's the computer's board, select a cell from the player's board
+      const selectedCell = selectCells(playerBoard);
+      // Check if the selected cell is a ship ('B')
+      if (playerBoard[selectedCell.coordX][selectedCell.coordY] === 'B') {
+        // Update the player's board cell state to 'hit' for a hit
+        playerBoard[selectedCell.coordX][selectedCell.coordY] = 'hit';
+      } else {
+        // Update the player's board cell state to 'miss' for a miss
+        playerBoard[selectedCell.coordX][selectedCell.coordY] = 'miss';
+      }
+      setPlayerBoard([...playerBoard]);
       setSelectedPlayerCell(selectedCell);
-      // You can use state to store the selected cell
-      // console.log(selectedPlayerCell)
-    } else {
-      setComputerBoard([...clickedBoard]);
     }
-
+    // Here, you can update the computer's board based on the result of the click if needed.
   };
-  return (
-    <>
-      <div className="controls-container">
-          <PlaceShipButton onClick={handlePlaceShips} shipsPlaced={shipsPlaced} />
-      </div>
-      <div className="main-container">
-        <ScoreBoard />
-        <ComputerBoard board={computerBoard} onCellClick={(rowIndex, colIndex) => handleCellClick(computerBoard, rowIndex, colIndex)} />
-        <PlayerBoard board={playerBoard} selectedCell={selectedPlayerCell} />
-      </div>
-  </>
 
+  return (
+    <div>
+      <ScoreBoard />
+      <ComputerBoard board={computerBoard} onCellClick={handleCellClick} />
+      <PlaceShipButton onClick={handlePlaceShips} shipsPlaced={shipsPlaced} />
+      <PlayerBoard board={playerBoard} selectedCell={selectedPlayerCell} />
+    </div>
   );
 }
-
-
-// const handleCellClick = (clickedBoard, rowIndex, colIndex) => {
-//   const selectedCell = selectCells(clickedBoard); // Select a cell on the player board
-//   console.log(selectedCell);
-//   // Check if the selected player cell is a ship ('B')
-//   if (selectedCell === 'B') {
-//     // Update the selected player cell state to 'hit' for a hit
-//     // and update the player board state
-//     clickedBoard[selectedCell.coordX][selectedCell.coordY] = 'hit';
-//   } else {
-//     // Update the selected player cell state to 'miss' for a miss
-//     // and update the player board state
-//     clickedBoard[selectedCell.coordX][selectedCell.coordY] = 'miss';
-//   }
-
-//   // Update the board state based on which board is being clicked
-//   if (clickedBoard === playerBoard) {
-//     setPlayerBoard([...clickedBoard]);
-//   } else {
-//     setComputerBoard([...clickedBoard]);
-//   }
-// };
-
-
